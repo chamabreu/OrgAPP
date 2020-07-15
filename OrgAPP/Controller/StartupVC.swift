637b8dc418da0dd2f33ee710321667b14c46c1f9
chamabreu <janmanuelbrenner@gmail.com>
2020-07-15 17:18:01 +0200

    Created LogInscreen for Multiuser. Textfields and Create User Button are dummys - LogIn Button Segues to Projects View Controller.

---
+++ OrgAPP/Controller/StartupVC.swift
@@ -0,0 +1,30 @@
+//
+//  StartupVC.swift
+//  OrgAPP
+//
+//  Created by Jan Manuel Brenner on 15.07.20.
+//  Copyright © 2020 Jan Manuel Brenner. All rights reserved.
+//
+
+import UIKit
+
+class StartupVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
